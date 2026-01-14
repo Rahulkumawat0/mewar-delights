5b21e2640f8074631d1099d55894314723a9a40b
Rahulkumawat0 <cse19_4919136@geeta.edu.in>
2026-01-14 16:45:07 +0530

    Fix case-sensitive naming for FoodCard component and CSS

--- client/src/pages/Menu.jsx
+++ client/src/pages/Menu.jsx
@@ -5,7 +5,6 @@ function Menu() {
   return (
     <div className="container my-5">
       <h2 className="text-center mb-4">Our Royal Menu</h2>
-
       <div className="row g-4">
         {foodData.map(food => (
           <div className="col-lg-3 col-md-4 col-sm-6" key={food.id}>
